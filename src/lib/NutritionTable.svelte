6ff9920b095de33048ee4d705045d396b67f8be9
Miguel <mige.d@outlook.com>
2022-04-04 21:51:49 -0400

    calculate total values in table

--- src/lib/NutritionTable.svelte
+++ src/lib/NutritionTable.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
   import { foods } from '../stores';
+
+  $: total_size = $foods
+    .map((food) => food.serving_size_g)
+    .reduce((a, b) => a + b, 0);
+  $: total_calories = $foods
+    .map((food) => food.calories)
+    .reduce((a, b) => a + b, 0);
+  $: total_carbs = $foods
+    .map((food) => food.carbohydrates_total_g)
+    .reduce((a, b) => a + b, 0);
+  $: total_fats = $foods
+    .map((food) => food.fat_total_g)
+    .reduce((a, b) => a + b, 0);
+  $: total_proteins = $foods
+    .map((food) => food.protein_g)
+    .reduce((a, b) => a + b, 0);
 </script>
 
-<div class="flex justify-center my-3 mx-3">
-  <table class="table table-zebra w-full max-w-4xl">
+<div class="flex justify-center m-3">
+  <table class="table w-full max-w-4xl">
     <thead>
       <tr>
         <th>Name</th>
@@ -18,13 +34,23 @@
       {#each $foods as food (food.name)}
         <tr>
           <td>{food.name}</td>
-          <td>{food.serving_size_g}g</td>
+          <td>{food.serving_size_g} g</td>
           <td>{food.calories}</td>
-          <td>{food.carbohydrates_total_g}g</td>
-          <td>{food.fat_total_g}g</td>
-          <td>{food.protein_g}g</td>
+          <td>{food.carbohydrates_total_g} g</td>
+          <td>{food.fat_total_g} g</td>
+          <td>{food.protein_g} g</td>
         </tr>
       {/each}
+      {#if $foods.length !== 0}
+        <tr class="active">
+          <td><strong>Total</strong></td>
+          <td>{total_size} g</td>
+          <td>{total_calories}</td>
+          <td>{total_carbs} g</td>
+          <td>{total_fats} g</td>
+          <td>{total_proteins} g</td>
+        </tr>
+      {/if}
     </tbody>
   </table>
 </div>
--- src/stores.ts
+++ src/stores.ts
@@ -2,8 +2,8 @@ import { writable } from 'svelte/store';
 
 interface Food {
   name: string;
-  calories: number;
   serving_size_g: number;
+  calories: number;
   carbohydrates_total_g: number;
   fat_total_g: number;
   protein_g: number;
